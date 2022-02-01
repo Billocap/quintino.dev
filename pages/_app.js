f39cc85ec750ff02e989c0bdaa48eb1356a69e8e
Gabriel Quintino <pikachurando@pm.me>
2022-02-01 14:49:01 -0300

    chore: security patch

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
 import Head from 'next/head';
 import { Fragment } from 'react';
 
-import 'styles/globals.css'
+import 'styles/_font.css';
+import 'styles/globals.css';
 
 export default function MyApp({ Component, pageProps }) {
 	return (
